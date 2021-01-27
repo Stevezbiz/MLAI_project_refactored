f31416bbfe2d42b5d44232be3ad7d9e945bb94b9
Stevezbiz <61883836+Stevezbiz@users.noreply.github.com>
2021-01-27 15:41:22 +0100

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -1,4 +1,4 @@
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from PIL import Image
 import skimage
 from torchvision.transforms import Compose, Resize, ToTensor, Normalize
